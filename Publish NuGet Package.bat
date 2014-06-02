ffc1d6f6e1770d2c28362d22dabd47a81ea53cd7
Adam <mail@fetzenet.de>
2014-06-03 00:13:34 +0200

    Maintenance #CHANGE: Updated NuGet publishing batch file to search for msbuild.

--- Publish NuGet Package.bat
+++ Publish NuGet Package.bat
@@ -1,3 +1,8 @@
+:: search for msbuild and make it available
+set bb.build.msbuild.exe=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildPath=%%D
+set PATH=%PATH%;%msbuildPath%
+
 :: make sure we have a clean release build
 msbuild /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" AdamsLair.WinForms.sln
 
