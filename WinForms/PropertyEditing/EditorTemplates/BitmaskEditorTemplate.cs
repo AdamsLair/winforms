390db9f58b968468501d1bf28075f937a5ebd65f
yamailbox <yamailbox@gmail.com>
2016-10-01 19:42:32 +0200

    fixes AdamsLair/duality#284

--- WinForms/PropertyEditing/EditorTemplates/BitmaskEditorTemplate.cs
+++ WinForms/PropertyEditing/EditorTemplates/BitmaskEditorTemplate.cs
@@ -182,7 +182,9 @@ namespace AdamsLair.WinForms.PropertyEditing.Templates
 
 			Size dropDownSize = new Size(
 				this.rect.Width, 
-				Math.Min(this.dropdownHeight, this.dropdown.PreferredHeight));
+				Math.Min(
+                    this.dropdownHeight, 
+                    (this.dropdown.ItemHeight * this.dropdown.Items.Count()) + this.dropdown.Margin.Vertical));
 			
 			Point dropDownLoc = new Point(
 				this.rect.X + parentGrid.AutoScrollPosition.X,
